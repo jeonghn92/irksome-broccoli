a799e73ad63ab765d12e86569fd2e448d6a90832
jeonghnlim <jeonghn92@naver.com>
2015-12-16 22:55:26 +0900

    initial commit

--- public/javascripts/screate.js
+++ public/javascripts/screate.js
@@ -1,3 +1,45 @@
-$('#add .short button').click(function(){
-    $('#answer input.short').hide('fast');
-})
+function short(){
+  $('.short').click(function(){
+    $('p').append($('input.short').html());
+    $('#long').remove();
+    $('#single').remove();
+    $('#multi').remove();
+    $('button.addsingle').show('fast');
+    $('button.addmulti').show('fast');
+  });
+}
+function long(){
+  $('.long').click(function(){
+    $('#short').remove();
+    $('#long').append('fast');
+    $('#single').remove();
+    $('#multi').remove();
+    $('button.addsingle').show('fast');
+    $('button.addmulti').show('fast');
+  });
+}
+function single(){
+  $('.single').click(function(){
+    $('#short').remove();
+    $('#long').remove();
+    $('input.single').show('fast');
+    $('#multi').remove();
+    $('button.addsingle').show('fast');
+    $('button.addmulti').show('fast');
+  });
+}
+function multi(){
+  $('.multi').click(function(){
+    $('#short').remove();
+    $('#long').remove();
+    $('input.single').remove();
+    $('input.multi').show('fast');
+    $('button.addsingle').show('fast');
+    $('button.addmulti').show('fast');
+  });
+}
+function addsingle(){
+  $('button.addsingle').click(function(){
+    $('input.single').append($('input.single'));
+  });
+}
